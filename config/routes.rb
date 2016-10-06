9df9e1c7da760984648b71e4d0721cf86bacac9f
zacscodingclub <zbaston@gmail.com>
2016-10-06 13:43:25 -0400

    generate scaffold for UserStocks and join table

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :user_stocks
+  resources :user_stocks, except: [:show, :edit, :update]
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'

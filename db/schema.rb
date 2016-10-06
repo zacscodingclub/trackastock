49a01da14dea6ce3128700f6e9e93c615ce99d74
zacscodingclub <zbaston@gmail.com>
2016-10-06 13:25:51 -0400

    generat scaffold for UserStocks join model/table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161006121608) do
+ActiveRecord::Schema.define(version: 20161006172356) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20161006121608) do
     t.string   "ticker"
     t.string   "name"
     t.decimal  "last_price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_stocks", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "stock_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

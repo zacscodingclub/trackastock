b5bb5a335d9b2b2cb5a257b0998f84c9538a9cf4
zacscodingclub <zbaston@gmail.com>
2016-10-07 11:34:03 -0400

    add User#show action/route.  show User's friends

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
   def my_portfolio
     @user = current_user
     @user_stocks = @user.stocks
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'pages#index'
   resources :user_stocks, except: [:show, :edit, :update]
+  resources :users, only: [:show]
+  resources :friendships
   devise_for :users, :controllers => { :registrations => 'user/registrations' }
 
   get 'my-portfolio', to: 'users#my_portfolio'

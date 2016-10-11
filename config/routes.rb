63fe888fc349bd449104acf2b1e7980c31cd6581
zacscodingclub <zbaston@gmail.com>
2016-10-11 13:44:41 -0400

    build form for searching friends

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   root 'pages#index'
   resources :user_stocks, except: [:show, :edit, :update]
-  resources :users, only: [:show]
+
   resources :friendships
   devise_for :users, :controllers => { :registrations => 'user/registrations' }
 
+  get 'search-friends', to:'users#search'
+  post 'add-friend', to:'users#add_friend'
+  get 'profile', to: 'users#show'
   get 'my-portfolio', to: 'users#my_portfolio'
   get 'stock-search', to: 'stocks#search'
   get 'my-friends', to: 'users#friends'

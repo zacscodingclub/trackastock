38677bea999dfd84cc22cf6e64374ff45269b0a1
zacscodingclub <zbaston@gmail.com>
2016-10-11 13:50:15 -0400

    transfer stocksjs to friendsjs and replace text.

---
+++ app/assets/javascripts/friends.js
@@ -0,0 +1,26 @@
+var init_friend_lookup;
+
+init_friend_lookup = function() {
+    $('#friend-lookup-form').on('ajax:before', function() {
+        showSpinner();
+    });
+
+    $('#friend-lookup-form').on('ajax:success', function(event, data, status) {
+        hideSpinner();
+        $('#friend-lookup').replaceWith(data);
+
+        init_friend_lookup();
+    });
+
+    $('#friend-lookup-form').on('ajax:error', function(event, xhr, status, error) {
+        hideSpinner();
+        $('#friend-lookup-results').replaceWith('');
+        $('#friend-lookup-errors').replaceWith('Friend not found');
+
+        init_friend_lookup();
+    });
+}
+
+$(document).ready(function() {
+    init_friend_lookup();
+});
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,21 @@ class UsersController < ApplicationController
   end
 
   def friends
-    @user = current_user
+    @friends = current_user.friends
+  end
+
+  def add_friend
+
+  end
+
+  def search
+    @users = User.search(params[:search_param])
+
+    if @users
+      @users = current_user.except_current_user(@users)
+      render partial: 'friends/lookup'
+    else
+      render status: :not_found, nothing: true
+    end
   end
 end
